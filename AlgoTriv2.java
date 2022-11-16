2f57646e04b731b1d83f2753c0dddd073de22dfd
Sir. wolf <46008114+wolf75222@users.noreply.github.com>
2022-11-16 19:31:21 +0100

    v2 rechercheSeq et rechercheDicho

--- AlgoTriv2.java
+++ AlgoTriv2.java
@@ -1,20 +1,30 @@
 import java.lang.Math;
 
-public class AlgoTri {
+public class AlgoTriv2 {
 
     public static void main(String[] args) {
        
-        // Pour voir si les fonctions marchent
-        test(10, 0, 10,"selection");
-        test(10, 0, 10,"insertion");
-        test(10, 0, 10,"drapeau");
+        /* 
+         * "Dans les affichages j'en fait un affichetab(t) qui renvoie par exemple : 1,5,7,9.......11,47,72,.........250,256,260"
+         * Je n'ai pas compris ce que vous vouliez dire
+         */
 
-        System.out.println("----------------------");
+
+        // Pour voir si les fonctions marchent
+        test(10, 0, 10,1);
+        test(10, 0, 10,2);
+        test(10, 0, 10,3);
+
 
         //Pour voir les temps d'execution
-        grosTest(100000, 0, 100000,"selection");
-        grosTest(100000, 0, 100000,"insertion");
-        grosTest(100000, 0, 100000,"drapeau");
+        grosTest(100000, 0, 100000,1);
+        grosTest(100000, 0, 100000,2);
+        grosTest(100000, 0, 100000,3);
+        
+
+        int [] tab = new int[1000000000];
+        remplir(tab, 0, 100000);
+        testRecherche(tab, 10579968);
 
     }
     
@@ -22,7 +32,7 @@ public class AlgoTri {
     /* 
      * Algorithme de tri par sélection
      */
-        int i, j, min, tmp;
+        int i, j, min;
         for (i = 0; i < tab.length - 1; i++) {
             min = i;
             for (j = i + 1; j < tab.length; j++) {
@@ -30,10 +40,18 @@ public class AlgoTri {
                     min = j;
                 }
             }
-            tmp = tab[i];
-            tab[i] = tab[min];
-            tab[min] = tmp;
+            echange(tab, i, min);
         }
+    }
+
+
+    public static void echange(int[] tab, int i, int j){
+        /*
+         * échange les éléments d'indice i et j du tableau tab
+         */
+        int tmp = tab[i];
+        tab[i] = tab[j];
+        tab[j] = tmp;
     }
 
     public static void triInsertion(int[] tab) {
@@ -112,23 +130,28 @@ public class AlgoTri {
         return true;
     }
 
-    public static void trier(int[] tab, String algo) {
+    public static void trier(int[] tab, int algo) {
     /* 
      * Trie un tableau d'entiers selon l'algorithme spécifié
      */
+        //constante
+        final int SELECTION = 1;
+        final int INSERTION = 2;
+        final int DRAPEAU = 3;
+
         long debut = System.currentTimeMillis();
-        if (algo.equals("selection")) {
+        if (algo == SELECTION) { 
             triSelection(tab);
-        } else if (algo.equals("insertion")) {
+        } else if (algo == INSERTION) {
             triInsertion(tab);
-        } else if (algo.equals("drapeau")) {
+        } else if (algo == DRAPEAU)  {
             triDrapeau(tab);
         }
         long fin = System.currentTimeMillis();
         System.out.println("Temps d'exécution : " + (fin - debut) + " ms");
     }
 
-    public static void test(int taille, int min, int max, String algo) {
+    public static void test(int taille, int min, int max, int algo) {
     /* 
      * Teste l'algorithme spécifié sur un tableau d'entiers aléatoires
      */
@@ -140,9 +163,10 @@ public class AlgoTri {
         System.out.println("Tableau trié avec "+ algo + ": ");
         afficher(tab);
         System.out.println("Est trié : " + estTrie(tab));
+        System.out.println("----------------------");
     }
 
-    public static void grosTest(int taille, int min, int max, String algo) {
+    public static void grosTest(int taille, int min, int max, int algo) {
     /* 
     * Teste l'algorithme spécifié sur un tableau d'entiers aléatoires, sans affichage
     */
@@ -151,7 +175,57 @@ public class AlgoTri {
         trier(tab, algo);
         System.out.println("Tableau trié avec "+ algo);
         System.out.println("Est trié : " + estTrie(tab));
+        System.out.println("----------------------");
     }
 
+    public static void testRecherche(int[] tab, int val){
+        /*
+         * Teste les fonctions de recherche
+         */
+        long debut = System.currentTimeMillis();
+        System.out.println(rechercheDicho(tab, val));
+        long fin = System.currentTimeMillis();
+        System.out.println("Temps d'exécution de la recherche dichotomique : " + (fin - debut) + " ms");
+        debut = System.currentTimeMillis();
+        System.out.println(rechercheSeq(tab, val));
+        fin = System.currentTimeMillis();
+        System.out.println("Temps d'exécution de la recherche Séquentielle : " + (fin - debut) + " ms");
+        System.out.println("----------------------");
+    }
+
+
+
+
+    public static int rechercheSeq(int[] tab, int val) {
+        /*
+        * Recherche séquentielle
+        */
+            for (int i = 0; i < tab.length; i++) {
+                if (tab[i] == val) {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    
+    public static int rechercheDicho(int[] tab, int val) {
+    /*
+    * Recherche dichotomique
+    */
+        int min = 0;
+        int max = tab.length - 1;
+        int milieu;
+        while (min <= max) {
+            milieu = (min + max) / 2;
+            if (tab[milieu] == val) {
+                return milieu;
+            } else if (tab[milieu] < val) {
+                min = milieu + 1;
+            } else {
+                max = milieu - 1;
+            }
+        }
+        return -1;
+    }
 
 }
